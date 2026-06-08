4ac8df4c4c5e85c0416968f4683e6f657247ecb5
Kauan-01 <k.kantovick2008@gmail.com>
2026-06-08 18:01:28 -0300

    feat: candidate and jobPostings

---
+++ skillmatch.js
@@ -0,0 +1,33 @@
+const candidate = {
+    name: "Ana",
+    area: "Front-End",
+    skills: ["JavaScript", "GitHub", "Programming Logic", "Kanban"],
+    monthsOfExperience: 3
+};
+
+const jobPostings = [
+    {
+        id: 1,
+        company: "TechStart",
+        position: "Junior Front-End Developer",
+        requirements: ["JavaScript", "GitHub", "Programming Logic", "Objects"],
+        salary: 2800,
+        workStyle: "Remote"
+    },
+    {
+        id: 2,
+        company: "CodeLab",
+        position: "Front-End Internship",
+        requirements: ["JavaScript", "Kanban", "GitHub"],
+        salary: 1800,
+        workStyle: "Hybrid"
+    },
+    {
+        id: 3,
+        company: "WebSolutions",
+        position: "Junior JavaScript Developer",
+        requirements: ["JavaScript", "Arrays", "Objects", "Functions"],
+        salary: 3000,
+        workStyle: "On-site"
+    }
+];
